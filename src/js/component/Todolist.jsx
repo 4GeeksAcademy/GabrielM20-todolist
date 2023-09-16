5006ac8579a2d6863e28e7f7bdba22b85d73a859
Gabriel Montaño <134933872+GabrielM20-1@users.noreply.github.com>
2023-09-16 08:08:48 +0000

    todolist

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -1,8 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const Todo = () => {
+
+function Todo() {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
+
+  function handleInput(event) {
+    event.preventDefault();
+    if (inputValue.trim() !== "") {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
+    } else {
+      alert("El campo no puede estar vacío");
+    }
+  }
+
+  function handleRemoveItem(index) {
+    const newTodos = todos.filter((_, currentIndex) => currentIndex !== index);
+    setTodos(newTodos);
+  }
+
+  useEffect(() => {
+    const getTodo = async () => {
+      let url = "https://playground.4geeks.com/apis/fake/todos/user/";
+      let options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        if (response.ok) {
+          const data = await response.json();
+          setTodos(data);
+        } else {
+          console.log("Error", response.status, response.statusText);
+        }
+      } catch (error) {
+        console.error("Error al obtener los datos:", error);
+      }
+    };
+    getTodo();
+
+  }, []);
 
   return (
     <div className="container">
@@ -15,8 +57,7 @@ const Todo = () => {
             value={inputValue}
             onKeyDown={(e) => {
               if (e.key === "Enter") {
-                setTodos([...todos, inputValue]);
-                setInputValue("");
+                handleInput(e);
               }
             }}
             placeholder="What do you need to do?"
@@ -28,8 +69,7 @@ const Todo = () => {
             <i
               className="fa solid fa-trash"
               onClick={() => {
-                const newTodos = todos.filter((_, currentIndex) => currentIndex !== index);
-                setTodos(newTodos);
+                handleRemoveItem(index);
               }}
             ></i>
           </li>
@@ -38,6 +78,6 @@ const Todo = () => {
       <div>{todos.length} items</div>
     </div>
   );
-};
+}
 
 export default Todo;
